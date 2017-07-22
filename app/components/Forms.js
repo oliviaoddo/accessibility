23c5a3cd171c94f98a369bd34175655945cfba9d
Olivia Oddo <olivia.oddo@gmail.com>
2017-07-22 16:59:31 -0400

    added information to form component

--- app/components/Forms.js
+++ app/components/Forms.js
@@ -21,25 +21,103 @@ export default function Forms(){
                   <div className="card">
                     <div className="card-content">
                     <form>
-                        <input placeholder='First Name'/>
-                        <input placeholder='Last Name'/>
-                        <input placeholder='Email'/>
+                        <fieldset>
+                        <legend>Account Information</legend>
+                        <div className='input-field'>
+                            <input className='validate' type='text' name ='first_name' id='fname' />
+                            <label  data-error="Invalid input. No Special characters." data-success="Valid Input" htmlFor='fname'>First Name</label>
+                        </div>
+                        <div className='input-field'>
+                            <input className='validate' type='text' name ='last_name' id='lname' />
+                            <label  data-error="Invalid input. No Special characters." data-success="Valid Input"htmlFor='lname'>Last Name</label>
+                        </div>
+                        <div className='input-field'>
+                            <input className='validate' type='email' name ='email' id='emailInput' required aria-required="true"/>
+                            <label data-error="Invalid Email Address. Follow format example@gmail.com" data-success="Valid Input" htmlFor='emailInput'>Email (required)</label>
+                        </div>
+                        <div className='input-field'>
+                            <input className='validate' type='text' name ='birthday' id='birthday' pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d" required aria-required="true" />
+                            <label data-error="Invalid input. Follow format mm/dd/yyy" data-success="Valid Input" htmlFor='birthday'>Birth Date (MM/DD/YYYY) (required)</label>
+                        </div>
+                        </fieldset>
+                        <fieldset>
+                        <legend>Campus Location</legend>
+                        <div className='input-field'>
+                            <input className='with-gap' type='radio' name ='campus' id='nyc' value='nyc' defaultChecked/>
+                            <label htmlFor='nyc'>New York City</label>
+                        </div>
+                        <div className='input-field'>
+                           <input className='with-gap' type='radio' name ='campus' id='sf' value='sf'/>
+                            <label htmlFor='sf'>San Francisco</label>
+                        </div>
+                        <div className='input-field'>
+                            <input className='with-gap' type='radio' name ='campus' id='chi' value='chi'/>
+                            <label htmlFor='chi'>Chicago</label>
+                        </div>
+                        </fieldset>
+                        <button className='btn pink darken-4 submitButton' type='submit'>Submit</button>
                     </form>
-                      <Highlight className='html'>
-                        {"<img src='brooklyn-bridge.jpg' alt='Brooklyn Bridge: New York City'/>"}
-                    </Highlight>
                     </div>
                   </div>
                 </div>
                 <div className="col m6">
+                  <div className="card">
+                    <div className="card-content">
+                        <Highlight className='html'>
+                            {"<img src='brooklyn-bridge.jpg'/>"}
+                        </Highlight>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col m6">
                  <span className='failTop'>Incorrect <i className="fa fa-times" aria-hidden="true"></i></span>
                   <div className="card">
                     <div className="card-content">
-                        <form>
-                            <input placeholder='First Name'/>
-                            <input placeholder='Last Name'/>
-                            <input placeholder='Email'/>
-                        </form>
+                    <form>
+                        <fieldset>
+                        <legend>Account Information</legend>
+                        <div className='input-field'>
+                            <input className='validate' type='text' name ='first_name' id='fname' />
+                            <label  data-error="Invalid input. No Special characters." data-success="Valid Input" htmlFor='fname'>First Name</label>
+                        </div>
+                        <div className='input-field'>
+                            <input className='validate' type='text' name ='last_name' id='lname' />
+                            <label  data-error="Invalid input. No Special characters." data-success="Valid Input"htmlFor='lname'>Last Name</label>
+                        </div>
+                        <div className='input-field'>
+                            <input className='validate' type='email' name ='email' id='emailInput' required aria-required="true"/>
+                            <label data-error="Invalid Email Address. Follow format example@gmail.com" data-success="Valid Input" htmlFor='emailInput'>Email (required)</label>
+                        </div>
+                        <div className='input-field'>
+                            <input className='validate' type='text' name ='birthday' id='birthday' pattern="(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d" required aria-required="true" />
+                            <label data-error="Invalid input. Follow format mm/dd/yyy" data-success="Valid Input" htmlFor='birthday'>Birth Date (MM/DD/YYYY) (required)</label>
+                        </div>
+                        </fieldset>
+                        <fieldset>
+                        <legend>Campus Location</legend>
+                        <div className='input-field'>
+                            <input className='with-gap' type='radio' name ='campus' id='nyc' value='nyc' defaultChecked/>
+                            <label htmlFor='nyc'>New York City</label>
+                        </div>
+                        <div className='input-field'>
+                           <input className='with-gap' type='radio' name ='campus' id='sf' value='sf'/>
+                            <label htmlFor='sf'>San Francisco</label>
+                        </div>
+                        <div className='input-field'>
+                            <input className='with-gap' type='radio' name ='campus' id='chi' value='chi'/>
+                            <label htmlFor='chi'>Chicago</label>
+                        </div>
+                        </fieldset>
+                        <button className='btn pink darken-4 submitButton' type='submit'>Submit</button>
+                    </form>
+                    </div>
+                  </div>
+                </div>
+                <div className="col m6">
+                  <div className="card">
+                    <div className="card-content">
                         <Highlight className='html'>
                             {"<img src='brooklyn-bridge.jpg'/>"}
                         </Highlight>
