cbe0dc570ee4f20890518e88d09e837f5c3defb3
Olivia Oddo <olivia.oddo@gmail.com>
2017-07-24 00:23:36 -0400

    removed database

--- server/api.js
+++ server/api.js
@@ -1,6 +1,5 @@
 'use strict'
-const api = require('express').Router()
-const db = require('../db')
+const api = require('express').Router();
 
 // If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
 	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
