755cfe164f1a9f95b8c97cba55cf58987d79d2cc
Olivia Oddo <olivia.oddo@gmail.com>
2017-07-24 00:42:45 -0400

    production

--- server/start.js
+++ server/start.js
@@ -7,11 +7,12 @@ const {resolve} = require('path')
 const pkg = require('../package.json')
 
 const app = express()
+process.env.NODE_ENV = 'production';
 
 if (process.env.NODE_ENV !== 'production') {
   // Logging middleware (non-production only)
   app.use(require('volleyball'))
-}  
+}
 
 //The code below works because `.use` returns `this` which is `app`. So what we want to return in the `module.exports` is `app`, and we can chain on that declaration because each method invokation returns `app` after mutating based on the middleware functions
 module.exports = app
@@ -26,11 +27,11 @@ module.exports = app
 if (module === require.main) {
   // Start listening only if we're the main module.
 
-  /* 
+  /*
     https://nodejs.org/api/modules.html#modules_accessing_the_main_module
       - This (module === require.main) will be true if run via node foo.js, but false if run by require('./foo')
-      - If you want to test this, log `require.main` and `module` in this file and also in `api.js`. 
-        * Note how `require.main` logs the same thing in both files, because it is always referencing the "main" import, where we starting running in Node 
+      - If you want to test this, log `require.main` and `module` in this file and also in `api.js`.
+        * Note how `require.main` logs the same thing in both files, because it is always referencing the "main" import, where we starting running in Node
         * In 'start.js', note how `module` is the same as `require.main` because that is the file we start with in our 'package.json' -- `node server/start.js`
         * In 'api.js', note how `module` (this specific file - i.e. module) is different from `require.main` because this is NOT the file we started in and `require.main` is the file we started in
           ~ To help compare these objects, reference each of their `id` attributes
@@ -38,7 +39,7 @@ if (module === require.main) {
   const server = app.listen(
     process.env.PORT || 1337,
     () => {
-      console.log(`--- Started HTTP Server for ${pkg.name} ---`)      
+      console.log(`--- Started HTTP Server for ${pkg.name} ---`)
       console.log(`Listening on ${JSON.stringify(server.address())}`)
     }
   )
